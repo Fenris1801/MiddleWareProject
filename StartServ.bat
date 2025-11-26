0a85337fbc7251a38c4376ac7614888a39a6c34c
Ludoclt <clolotludovic@gmail.com>
2025-11-26 20:31:27 +0100

    docker runtime environment

--- StartServ.bat
+++
@@ -1,7 +0,0 @@
-cd /d "%~dp0"
-
-start "" ".\backend\LetsGoBiking\bin\Debug\LetsGoBiking.exe"
-start "" ".\backend\ProxyCache\bin\Debug\ProxyCache.exe"
-start "" python -m http.server 5500 --directory ".\front"
-
-pause
